3daabde4018aab760c4eea3141116127c83faddf
Igor <i.pantea24@imperial.ac.uk>
2025-01-23 15:51:48 +0000

    Create initial package

---
+++ R/hello.R
@@ -0,0 +1,39 @@
+# usethis::use_gpl3_license()
+# usethis::use_mit_license()
+
+# devtools::load_all(".")
+
+#' Square a number
+#'
+#' Computes the square of a number
+#'
+#' @param x A number to be squared
+#'
+#' @return A squared number
+#' @export
+#'
+#' @examples
+#' square(10)
+
+square <- function(x) {
+  x * x * x
+}
+
+# devtools::document()
+
+# ?square
+# ?resid
+
+# devtools:: uses_testthat()
+
+# usethis::use_test()
+
+# usethis::use_r
+
+square_say <- function(x){
+  ans <- square(x)
+  str <- sprintf("The square of %s is %s", x, ans)
+  cowsay::say(str, by = "cow")
+}
+
+# devtools::load_all(".")
